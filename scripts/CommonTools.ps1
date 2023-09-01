cd6bb535deabb309826a2947af60332e3edb51d7
dethkube <destroyer298@gmail.com>
2023-08-31 21:32:20 -0700

    First Commit

---
+++ scripts/CommonTools.ps1
@@ -0,0 +1,10 @@
+
+# tools we expect users across many scenarios will want
+choco install -y notepadplusplus.install
+choco install -y 7zip.install
+choco install -y vlc.install
+choco install -y wiztree
+choco install -y crystaldiskinfo
+choco install -y microsoft-windows-terminal
+choco install -y deluge
+choco install -y virtualclonedrive
