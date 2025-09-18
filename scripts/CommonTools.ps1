2f07fa9fe07da2a1684bfa65edefd2ab12db1c46
Eric Sobel <destroyer298@gmail.com>
2025-09-17 21:48:16 -0700

    Add a lot of default apps

--- scripts/CommonTools.ps1
+++ scripts/CommonTools.ps1
@@ -1,10 +1,29 @@
 
 # tools we expect users across many scenarios will want
 choco install -y notepadplusplus.install
+choco install -y autohotkey.install
+
 choco install -y 7zip.install
 choco install -y vlc.install
+choco install -y mpvio.install
+choco install -y foobar2000
+choco install -y freeencoderpack
+
+choco install -y nicotine-plus
+choco install -y mp3tag
+
+choco install -y audacity
+choco install -y audacity-lame
+choco install -y audacity-ffmpeg
+
 choco install -y wiztree
+choco install -y hwinfo.install
 choco install -y crystaldiskinfo
+choco install -y crystaldiskmark
 choco install -y microsoft-windows-terminal
 choco install -y virtualclonedrive
-# choco install -y deluge # old version, doesn't work
+
+choco install -y pia
+choco install -y deluge
+
+choco install -y foxitreader
