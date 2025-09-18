cf96e66799b89ec5501e60c7810a398c920fbfb0
Eric Sobel <destroyer298@gmail.com>
2025-09-17 21:31:51 -0700

    Create Win11Debloat.ps1

---
+++ scripts/Win11Debloat.ps1
@@ -0,0 +1,6 @@
+#--- Declutters and improves your Windows experience ---
+
+# Reference:
+# https://github.com/Raphire/Win11Debloat
+
+& ([scriptblock]::Create((irm "https://debloat.raphi.re/"))) -RunDefaults -Silent
